477e7916c4123b5cf875ae2fb522a92b71636b2f
AngelClover <AngelClover@Excalibur.local>
2016-02-04 20:14:03 +0800

    test for 100

---
+++ sgu/100/100.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# 
+########################################################################
+ 
+"""
+File: 100.py
+Author: AngelClover(AngelClover@aliyun.com)
+Date: 2016/02/04 20:02:56
+"""
+import sys
+
+if __name__ == "__main__":
+    l = sys.stdin.readline().split()
+    print int(l[0]) + int(l[1])
